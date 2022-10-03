0398e44d507e178d59588844a0aeb18e3e8a4192
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2022-10-03 21:10:17 +0100

    Typescript Is So Cool | Other

---
+++ src/Typescript/cuties.ts
@@ -0,0 +1,6 @@
+// not the best way but im dumb
+// Paup
+document.getElementById('Paup')!.setAttribute('src', 'https://cdn.discordapp.com/avatars/491492268793004035/a0795e08682359a3f80502ac56b1fd20.png?size=1024&quality=lossless')!;
+
+// Alex
+document.getElementById('Alex')!.setAttribute('src', 'https://cdn.discordapp.com/avatars/650077100765609990/140ce3b5c50297d89726e63b76184295.png?size=1024&quality=lossless')!;
--- src/Typescript/nav.ts
+++ src/Typescript/nav.ts
@@ -2,7 +2,7 @@ fetch('./src/html/nav.html')
     .then(function (res) { return res.text(); })
     .then(function (text) {
     var oldelem = document.querySelector("script#replace_with_navbar")!;
-    var newelem = document.createElement("div")!;
+    var newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode?.replaceChild(newelem, oldelem);
 });
