dc273f8e32ab82a5566f815eca8e4188fb1e2b5d
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2022-11-08 20:58:06 +0000

    Funny

--- src/JavaScript/cuties.js
+++ src/JavaScript/cuties.js
@@ -1,6 +1,6 @@
 "use strict";
 // not the best way but im Lazy
 // Paup
-document.getElementById('Paup').setAttribute('src', 'https://cdn.discordapp.com/avatars/491492268793004035/876ede5794bff4cbc8aa160884ab08b7.png?size=1024&quality=lossless');
-// Alex
+document.getElementById('Paup').setAttribute('src', 'https://cdn.discordapp.com/avatars/491492268793004035/a0795e08682359a3f80502ac56b1fd20.png?size=1024&quality=lossless');
 document.getElementById('Alex').setAttribute('src', 'https://cdn.discordapp.com/avatars/650077100765609990/140ce3b5c50297d89726e63b76184295.png?size=1024&quality=lossless');
+document.getElementById('a').setAttribute('src', 'https://cdn.discordapp.com/avatars/707228048864444477/41e998c018675bfc26d40c1f55a3f225.png?size=1024');
