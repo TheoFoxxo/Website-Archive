9accb8ea7ec0fe9f63123f0bb672b5212cf29ff3
HackDev <113368165+TheodoreTheFox@users.noreply.github.com>
2023-01-15 09:41:01 +0000

    e

--- src/JavaScript/nav.js
+++ src/JavaScript/nav.js
@@ -8,13 +8,4 @@ fetch('/./src/html/nav.html')
     newelem.innerHTML = text;
     (_a = oldelem.parentNode) === null || _a === void 0 ? void 0 : _a.replaceChild(newelem, oldelem);
 });
-fetch('/./src/html/smart.html')
-    .then(function (res) { return res.text(); })
-    .then(function (text) {
-    var _a;
-    var oldelem = document.querySelector("script#smart_ass");
-    var newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    (_a = oldelem.parentNode) === null || _a === void 0 ? void 0 : _a.replaceChild(newelem, oldelem);
-});
 
